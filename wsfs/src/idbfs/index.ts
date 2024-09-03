8e558dbea0fe575801a4a44ed4f9ad74ab944051
Shaun Keys <mariobuddy@gmail.com>
2024-09-02 19:14:04 -0600

    Add most metadata functions

--- wsfs/src/idbfs/index.ts
+++ wsfs/src/idbfs/index.ts
@@ -1,5 +1,5 @@
 import { ObjStoreWrapper } from "./idbWrappers";
-import { Aggregation, Chunk, FileInode, FileType, Inode } from "./types";
+import { Aggregation, Chunk, FileType, Inode, NodeStat } from "./types";
 
 /**
  * Opens a filesystem backed by IndexedDB
@@ -16,13 +16,36 @@ function openIdbFs(name: string): Promise<IdbFs> {
 				autoIncrement: true,
 				keyPath: "id",
 			}));
+
+			const now = Date.now();
+
 			inodes.add({
 				id: 0,
-				type: FileType.DT_DIR,
-				parent: 0, // `/../` is the same as `/`
+				type: FileType.Special,
+				rdev: 0,
+				lookups: 0,
+				deleted: false,
+				generation: -1, // Not representable in u64. Indicates placeholder.
+				ctime: now,
+				uid: 0,
+				gid: 0,
+				mode: 0o755,
+				xattrs: new Map(),
+			});
+			inodes.add({
+				id: 1,
+				type: FileType.Directory,
+				lookups: 0,
+				deleted: false,
+				generation: gengen(),
+				parent: 1, // `/../` is the same as `/`
+				ctime: now,
+				mtime: now,
+				uid: 0,
+				gid: 0,
 				mode: 0o755,
 				subdirs: new Map(),
-				xattrs: {},
+				xattrs: new Map(),
 			});
 
 			// Data aggregation instructions
@@ -52,7 +75,23 @@ function openIdbFs(name: string): Promise<IdbFs> {
  */
 class FsError extends Error { }
 
-const defaultPermissions = 0o0666;
+/** The size of each chunk */
+const defaultBlockSize = 256;
+
+/** bitmask for file type inside mode */
+const S_IFMT = 0o170000;
+const S_IFDIR = 0o040000;
+const S_IFCHR = 0o020000;
+const S_IFBLK = 0o060000;
+const S_IFREG = 0o100000;
+const S_IFIFO = 0o010000;
+const S_IFLNK = 0o120000;
+const S_IFSOCK = 0o140000;
+
+/** Generates a random `generation` number */
+function gengen(): number {
+	return Math.round(Math.random() * Number.MAX_SAFE_INTEGER);
+}
 
 /**
  * An interface to a filesystem backed by IndexedDB.
@@ -62,98 +101,440 @@ class IdbFs {
 	/** inodes that need to be cleaned up once they are closed */
 	unlinkedInodes: number[];
 
+	blockSize: number;
+
 	constructor(database: IDBDatabase) {
 		this.db = database;
 		this.unlinkedInodes = [];
+		this.blockSize = defaultBlockSize; // May be configurable later. We'll see...
+	}
+
+	async lookup(parent: number, name: string): Promise<NodeStat> {
+		const transaction = this.db.transaction(["inodes"], "readwrite");
+		const inodeStore = new ObjStoreWrapper<Inode>(transaction.objectStore("inodes"));
+
+		const parentInode = await inodeStore.get(parent);
+		if (typeof parentInode === "undefined") {
+			throw new FsError("No such file or directory");
+		}
+		if (parentInode.type !== FileType.Directory) {
+			throw new FsError("No such file or directory");
+		}
+
+		const childInodeNum = parentInode.subdirs.get(name);
+		if (typeof childInodeNum !== "number") {
+			throw new FsError("No such file or directory");
+		}
+		const childInode = await inodeStore.get(childInodeNum);
+		if (typeof childInode === "undefined") {
+			throw new FsError("No such file or directory");
+		}
+		childInode.lookups += 1;
+		await inodeStore.put(childInode);
+
+		return await this.getattr(childInodeNum);
+	}
+
+	async forget(inodeNum: number, nlookup: number) {
+		// Decrement lookup count by nlookup and delete inode if it is staged for deletion
+		const transaction = this.db.transaction(["inodes", "aggs", "chunks"], "readwrite");
+		const inodeStore = new ObjStoreWrapper<Inode>(transaction.objectStore("inodes"));
+		const aggStore = new ObjStoreWrapper<Aggregation>(transaction.objectStore("aggs"));
+		const chunkStore = new ObjStoreWrapper<Chunk>(transaction.objectStore("chunks"));
+
+		const inode = await inodeStore.get(inodeNum);
+		if (typeof inode === "undefined") return;
+		inode.lookups -= nlookup;
+
+		// Delete data if unreferenced
+		if (inode.deleted) {
+			await inodeStore.delete(inodeNum);
+			if (inode.type === FileType.File) {
+				const agg = await aggStore.get(inode.aggId);
+				if (typeof agg !== "undefined") {
+					agg.linkedInodes -= 1;
+					if (agg.linkedInodes === 0) {
+						// < 0 bug checks done in GC because I want to know about them.
+						for (const chunk of agg.chunks) {
+							await chunkStore.delete(chunk);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	async getattr(inodeNum: number): Promise<NodeStat> {
+		const transaction = this.db.transaction(["inodes", "aggs"], "readonly");
+		const inodeStore = new ObjStoreWrapper<Inode>(transaction.objectStore("inodes"));
+		const aggStore = new ObjStoreWrapper<Aggregation>(transaction.objectStore("aggs"));
+
+		const inode = await inodeStore.get(inodeNum);
+		if (typeof inode === "undefined") {
+			// Inode? File descriptor? I think I'm gonna return EBADF here....
+			throw new FsError("Bad file descriptor");
+		}
+
+		let size: number;
+		let blocks: number;
+		let mtimeMs: number;
+		let nlink: number;
+		let rdev: number;
+		let blksize: number;
+
+		if (inode.type === FileType.File) {
+			const agg = await aggStore.get(inode.aggId);
+			if (typeof agg === "undefined") {
+				throw new FsError("Bad file descriptor");
+			}
+			size = agg.chunks.length * agg.chunksize - agg.trim;
+			blocks = agg.chunks.length;
+			mtimeMs = agg.mtime;
+			nlink = agg.linkedInodes;
+			rdev = 0;
+			blksize = agg.chunksize;
+		} else {
+			if (inode.type === FileType.Special) {
+				rdev = inode.rdev;
+			} else {
+				rdev = 0;
+			}
+			size = 0;
+			blocks = 0;
+			mtimeMs = inode.ctime;
+			nlink = 1;
+			blksize = this.blockSize;
+		}
+
+		return {
+		    ino: inode.id!,
+		    size,
+		    blocks,
+		    atimeMs: 0,
+		    mtimeMs,
+		    ctimeMs: inode.ctime,
+		    crtimeMs: inode.crtime,
+		    mode: inode.mode,
+		    nlink,
+		    uid: inode.uid,
+		    gid: inode.gid,
+		    rdev,
+		    blksize,
+		};
+	}
+
+	async setattr(
+	    ino: number,
+	    mode: number | null,
+	    uid: number | null,
+	    gid: number | null,
+	    size: number | null,
+	    mtime: number | null,
+	    ctime: number | null,
+	    crtime: number | null,
+	): Promise<NodeStat> {
+		const transaction = this.db.transaction(["inodes", "aggs"], "readwrite");
+		const inodeStore = new ObjStoreWrapper<Inode>(transaction.objectStore("inodes"));
+		const aggStore = new ObjStoreWrapper<Aggregation>(transaction.objectStore("aggs"));
+
+		const inode = await inodeStore.get(ino);
+		if (typeof inode === "undefined") {
+			throw new FsError("Bad file descriptor");
+		}
+
+		if (mode !== null) inode.mode = mode;
+		if (uid !== null) inode.uid = uid;
+		if (gid !== null) inode.gid = gid;
+		if (size !== null && inode.type === FileType.File) await this.truncateAgg(inode.aggId, size);
+		if (mtime !== null) {
+			if (inode.type === FileType.File) {
+				await this.setmtimeAgg(inode.aggId, mtime);
+			} else if(inode.type === FileType.Directory) {
+				inode.mtime = mtime;
+			}
+		}
+		if (ctime !== null) inode.ctime = ctime;
+		if (crtime !== null) inode.crtime = crtime;
+
+		await inodeStore.put(inode);
+
+		return await this.getattr(ino);
+	}
+
+	async readlink(ino: number): Promise<string> {
+		const transaction = this.db.transaction(["inodes"], "readonly");
+		const inodeStore = new ObjStoreWrapper<Inode>(transaction.objectStore("inodes"));
+
+		const inode = await inodeStore.get(ino);
+		if (typeof inode === "undefined") throw new FsError("No such file or directory");
+		if (inode.type !== FileType.Symlink) throw new FsError("Not a symlink");
+		return inode.target;
+	}
+
+	/** Create a new file */
+	async mknod(
+		uid: number,
+		gid: number,
+		parent: number,
+		name: string,
+		mode: number,
+		umask: number,
+		rdev: number,
+	): Promise<Inode> {
+		const transaction = this.db.transaction(["inodes", "aggs"], "readwrite");
+		const inodeStore = new ObjStoreWrapper<Inode>(transaction.objectStore("inodes"));
+		const aggStore = new ObjStoreWrapper<Aggregation>(transaction.objectStore("aggs"));
+
+		const parentInode = await inodeStore.get(parent);
+		if (typeof parentInode === "undefined") {
+			throw new FsError("No such file or directory");
+		}
+		if (parentInode.type !== FileType.Directory) {
+			throw new FsError("No such file or directory");
+		}
+		if (parentInode.subdirs.has(name)) {
+			throw new FsError("File or directory already exists");
+		}
+
+		const now = Date.now();
+
+		let inode: Inode;
+		switch (mode & S_IFMT) {
+			case S_IFDIR:
+				inode = {
+					type: FileType.Directory,
+					parent,
+					lookups: 0,
+					deleted: false,
+					generation: gengen(),
+					crtime: now,
+					ctime: now,
+					mtime: now,
+					mode: mode & (~umask),
+					gid,
+					uid,
+					subdirs: new Map(),
+					xattrs: new Map(),
+				};
+				break;
+			case S_IFLNK:
+				throw new FsError("Can't create link from mknod");
+			case S_IFREG:
+				const agg: Aggregation = {
+					chunksize: this.blockSize,
+					chunks: [],
+					linkedInodes: 1,
+					mtime: now,
+					trim: 0,
+				};
+				const aggId = await aggStore.add(agg);
+				inode = {
+					type: FileType.File,
+					lookups: 0,
+					deleted: false,
+					generation: gengen(),
+					crtime: now,
+					ctime: now,
+					mode: mode & (~umask),
+					gid,
+					uid,
+					aggId,
+					xattrs: new Map(),
+				};
+				break;
+			case S_IFBLK:
+			case S_IFCHR:
+			case S_IFIFO:
+			case S_IFSOCK:
+			default:
+				inode = {
+					type: FileType.Special,
+					lookups: 0,
+					deleted: false,
+					generation: gengen(),
+					crtime: now,
+					ctime: now,
+					mode: mode & (~umask),
+					gid,
+					uid,
+					rdev,
+					xattrs: new Map(),
+				};
+				break;
+		}
+
+		const inodeId = await inodeStore.add(inode);
+		parentInode.subdirs.set(name, inodeId);
+		parentInode.mtime = Date.now();
+		await inodeStore.put(parentInode);
+		return inode;
 	}
 
 	/** Create a new directory */
-	async mkdir(parent: number, name: string, mode: number): Promise<number> {
+	mkdir(uid: number, gid: number, parent: number, name: string, mode: number): Promise<Inode> {
+		return this.mknod(uid, gid, parent, name, (mode & (~S_IFMT)) | S_IFDIR, 0, 0);
+	}
+
+	/** Unlinks an inode from the filesystem */
+	private async unlinkAny(parent: number, name: string, rmdir: boolean) {
 		const transaction = this.db.transaction(["inodes"], "readwrite");
-
 		const inodeStore = new ObjStoreWrapper<Inode>(transaction.objectStore("inodes"));
 
 		const parentInode = await inodeStore.get(parent);
-		if (parentInode.type !== FileType.DT_DIR) {
-			throw new FsError("Parent must be a directory");
+		if (typeof parentInode === "undefined") {
+			throw new FsError("No such file or directory");
 		}
-		if (name in parentInode.subdirs) {
-			throw new FsError("Directory already exists");
+		if (parentInode.type !== FileType.Directory) {
+			throw new FsError("No such file or directory");
 		}
 
-		const inode: Inode = {
-			type: FileType.DT_DIR,
-			mode,
+		const inodeNum = parentInode.subdirs.get(name);
+		if (typeof inodeNum !== "number") {
+			throw new FsError("No such file or directory");
+		}
+
+		const inode = await inodeStore.get(inodeNum);
+		if (typeof inode !== "undefined") {
+			if (inode.type === FileType.Directory) {
+				if (!rmdir) {
+					throw new FsError("Cannot unlink directory");
+				}
+				if (inode.subdirs.size) {
+					throw new FsError("Cannot delete non-empty directory");
+				}
+			} else {
+				if (rmdir) {
+					throw new FsError("Not a directory");
+				}
+			}
+			inode.deleted = true;
+			await inodeStore.put(inode);
+			this.forget(inodeNum, 0);
+		}
+
+		parentInode.subdirs.delete(name);
+		await inodeStore.put(parentInode);
+	}
+
+	/** Unlinks a file */
+	unlink(parent: number, name: string) {
+		return this.unlinkAny(parent, name, false);
+	}
+
+	/** Unlinks a directory (must be empty) */
+	rmdir(parent: number, name: string) {
+		return this.unlinkAny(parent, name, true);
+	}
+
+	async symlink(uid: number, gid: number, parent: number, linkName: string, target: string): Promise<NodeStat> {
+		const transaction = this.db.transaction(["inodes"], "readwrite");
+		const inodeStore = new ObjStoreWrapper<Inode>(transaction.objectStore("inodes"));
+
+		const parentInode = await inodeStore.get(parent);
+		if (typeof parentInode === "undefined" || parentInode.type !== FileType.Directory) {
+			throw new FsError("No such file or directory");
+		}
+		if (parentInode.subdirs.has(linkName)) {
+			throw new FsError("File already exists");
+		}
+
+		const now = Date.now();
+
+		const symlinkIno = await inodeStore.add({
+			type: FileType.Symlink,
 			parent,
-			subdirs: {},
-			xattrs: {},
-		};
-		const inodeId = await inodeStore.add(inode);
+			lookups: 0,
+			deleted: false,
+			generation: gengen(),
+			crtime: now,
+			ctime: now,
+			mode: S_IFLNK | 0o777,
+			gid,
+			uid,
+			xattrs: new Map(),
+			target,
+		});
 
-		parentInode.subdirs.set(name, inodeId);
-		await inodeStore.add(parentInode);
+		parentInode.subdirs.set(linkName, symlinkIno);
+		parentInode.mtime = Date.now();
+		await inodeStore.put(parentInode);
 
-		transaction.commit();
-		return inodeId;
+		return await this.getattr(symlinkIno);
 	}
 
-	/** Link tmpfile to filesystem */
-	async linkFile(file: number, parent: number, name: string) {
-		const transaction = this.db.transaction(["inodes", "aggs", "chunks"], "readwrite");
-
+	async rename(parent: number, name: string, newparent: number, newname: string): Promise<void> {
+		const transaction = this.db.transaction(["inodes"], "readwrite");
 		const inodeStore = new ObjStoreWrapper<Inode>(transaction.objectStore("inodes"));
-		const aggStore = new ObjStoreWrapper<Aggregation>(transaction.objectStore("aggs"));
-		const chunkStore = new ObjStoreWrapper<Chunk>(transaction.objectStore("chunks"));
 
 		const parentInode = await inodeStore.get(parent);
-		if (parentInode.type !== FileType.DT_DIR) {
+		if (typeof parentInode === "undefined" || parentInode.type !== FileType.Directory) {
 			throw new FsError("No such file or directory");
 		}
-		let olddir = parentInode.subdirs.get(name);
-		parentInode.subdirs.set(name, file);
-		await inodeStore.add(parentInode);
-
-		if (typeof olddir === "number") {
-			// TODO: Clean up defunct inode
-		}
-	}
-
-	/** Unlinks a file from the filesystem */
-	async unlinkFile(parent: number, name: string) {
-		const transaction = this.db.transaction(["inodes", "aggs", "chunks"], "readwrite");
-
-		const inodeStore = new ObjStoreWrapper<Inode>(transaction.objectStore("inodes"));
-		const aggStore = new ObjStoreWrapper<Aggregation>(transaction.objectStore("aggs"));
-		const chunkStore = new ObjStoreWrapper<Chunk>(transaction.objectStore("chunks"));
-
-		const parentInode = await inodeStore.get(parent);
-		if (parentInode.type !== FileType.DT_DIR) {
+		const ino = parentInode.subdirs.get(name);
+		if (typeof ino === "undefined") {
 			throw new FsError("No such file or directory");
 		}
+		const inode = await inodeStore.get(ino);
+		if (typeof inode === "undefined") {
+			throw new FsError("No such file or directory");
+		}
+
+		const newparentInode = await inodeStore.get(newparent);
+		if (typeof newparentInode === "undefined" || newparentInode.type !== FileType.Directory) {
+			throw new FsError("No such file or directory");
+		}
+
+		if (inode.type === FileType.Directory) {
+			inode.parent = newparent;
+			await inodeStore.put(inode);
+		}
+		const time = Date.now();
+		parentInode.subdirs.delete(name);
+		parentInode.mtime = time;
+		newparentInode.subdirs.set(newname, ino);
+		newparentInode.mtime = time;
+		await Promise.all([
+			inodeStore.put(parentInode),
+			inodeStore.put(newparentInode),
+		]);
 	}
 
-	/** Create a temporary file */
-	async createTmpFile(mode: number): Promise<number> {
-		const transaction = this.db.transaction(["inodes", "aggs", "chunks"], "readwrite");
-
-		const agg: Aggregation = {
-			chunks: [],
-			linkedInodes: 1,
-		};
-		const aggStore = new ObjStoreWrapper<Aggregation>(transaction.objectStore("aggs"));
-		const aggId = await aggStore.add(agg);
-
-		const inode: Inode = {
-			type: FileType.DT_REG,
-			mode,
-			xattrs: {},
-			aggId,
-		};
+	async link(uid: number, gid: number, ino: number, newparent: number, newname: string): Promise<NodeStat> {
+		const transaction = this.db.transaction(["inodes", "aggs"], "readwrite");
 		const inodeStore = new ObjStoreWrapper<Inode>(transaction.objectStore("inodes"));
-		const inodeId = await inodeStore.add(inode);
+		const aggStore = new ObjStoreWrapper<Aggregation>(transaction.objectStore("aggs"));
 
-		transaction.commit();
-		return inodeId;
+		const inode = await inodeStore.get(ino);
+		if (typeof inode === "undefined") {
+			throw new FsError("No such file or directory");
+		}
+		if (inode.type !== FileType.File) {
+			throw new FsError("Can only hard-link regular files");
+		}
+		const newparentInode = await inodeStore.get(newparent);
+		if (typeof newparentInode === "undefined" || newparentInode.type !== FileType.Directory) {
+			throw new FsError("No such file or directory");
+		}
+		if (newparentInode.subdirs.has(newname)) {
+			throw new FsError("File already exists");
+		}
+
+		const now = Date.now();
+		const newIno = await inodeStore.add({
+			type: FileType.File,
+			lookups: 0,
+			deleted: false,
+			generation: gengen(),
+			crtime: now,
+			ctime: now,
+			mode: inode.mode,
+			gid,
+			uid,
+			xattrs: new Map(),
+			aggId: inode.aggId,
+		})
+
+		newparentInode.subdirs.set(newname, newIno);
 	}
 }
 
--- wsfs/src/idbfs/types.ts
+++ wsfs/src/idbfs/types.ts
@@ -3,13 +3,10 @@
  * to system-specific ones from libc.
  */
 export enum FileType {
-	DT_BLK     = 1,
-	DT_CHR     = 2,
-	DT_DIR     = 3,
-	DT_FIFO    = 4,
-	DT_LNK     = 5,
-	DT_REG     = 6,
-	DT_SOCK    = 7,
+	Special     = 0,
+	Directory   = 1,
+	Symlink     = 2,
+	File        = 3,
 }
 
 /** Common properties of all inodes */
@@ -23,52 +20,60 @@ export interface BaseInode {
 	 * TODO: Add lookup increments to functions.
 	 */
 	lookups: number,
+	/**
+	 * inode ID / generation number combo must be unique for the duration of the filesystem.
+	 */
+	generation: number,
+	deleted: boolean,
+
+	/** Time (in ms) that the inode's metadata was last modified */
+	ctime: number,
+
+	/** Time that the inode was created */
+	crtime: number,
+
+	uid: number,
+	gid: number,
 	mode: number,
-	xattrs: Record<string, Uint8Array>,
+
+	xattrs: Map<string, Uint8Array>,
 }
 
 /** Represents a file of any kind */
 export type Inode =
+	| SpecialInode
 	| FileInode
 	| DirInode
 	| LinkInode
-	| DevInode
-	| IpcInode
 	;
+
+/** Special Files */
+export interface SpecialInode extends BaseInode {
+	type: FileType.Special,
+	rdev: number,
+}
 
 /** Regular Files */
 export interface FileInode extends BaseInode {
-	type: FileType.DT_REG,
-	aggId: IDBValidKey,
+	type: FileType.File,
+	aggId: number,
 }
 
 /** Directories */
 export interface DirInode extends BaseInode {
-	type: FileType.DT_DIR,
+	type: FileType.Directory,
 	parent: number,
 	/** filename -> inode */
 	subdirs: Map<string, number>,
+	mtime: number,
 }
 
 /** Symbolic Links */
 export interface LinkInode extends BaseInode {
-	type: FileType.DT_LNK,
+	type: FileType.Symlink,
 	parent: number,
 	target: string,
 }
-
-/** Device files */
-export interface DevInode extends BaseInode {
-	type: FileType.DT_BLK | FileType.DT_CHR,
-	major: number,
-	minor: number,
-}
-
-/** Special files handled by the kernel. These just need a place in the tree */
-export interface IpcInode extends BaseInode {
-	type: FileType.DT_FIFO | FileType.DT_SOCK,
-}
-
 
 /**
  * Aggregates chunks outside the inode to allow for easy hard-links.
@@ -83,12 +88,18 @@ export interface Aggregation {
 	 * should be deleted.
 	 */
 	linkedInodes: number,
+	/** Last modified time (in ms) */
+	mtime: number,
+	/** The size of each chunk in `chunks` */
+	chunksize: number,
+	/** The number of bytes to trim from the end of the last chunk */
+	trim: number,
 	/**
 	 * `(size, chunkId)`
 	 *
 	 * Allows calculation of which chunks are needed without fetching linked-list style
 	 */
-	chunks: Array<[number, IDBValidKey]>,
+	chunks: Array<number>,
 }
 
 /**
@@ -97,4 +108,20 @@ export interface Aggregation {
 export interface Chunk {
 	id?: number,
 	data: Uint8Array,
+}
+
+export interface NodeStat {
+    ino: number,
+    size: number,
+    blocks: number,
+    atimeMs: number,
+    mtimeMs: number,
+    ctimeMs: number,
+    crtimeMs: number,
+    mode: number,
+    nlink: number,
+    uid: number,
+    gid: number,
+    rdev: number,
+    blksize: number,
 }
