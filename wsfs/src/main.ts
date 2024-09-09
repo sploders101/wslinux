46ae7c567b9334296861c0855d144120b5c98972
Shaun Keys <mariobuddy@gmail.com>
2024-09-08 22:53:03 -0600

    Fix demo & use lookup instead of readdir

--- wsfs/src/main.ts
+++ wsfs/src/main.ts
@@ -1,7 +1,8 @@
 import './style.css'
 
-import { IdbFs, S_IFREG, openIdbFs } from "./idbfs";
+import { IdbFs, S_IFMT, S_IFREG, openIdbFs } from "./idbfs";
 import { connectFilesystem } from './idbfs-connector';
+import { Entry } from './idbfs/types';
 
 async function writeDemo(fs: IdbFs) {
 	const textContents = [
@@ -18,23 +19,26 @@ async function writeDemo(fs: IdbFs) {
 	const contents = encoder.encode(textContents);
 
 	const file = await fs.mknod(0, 0, 1, "readme.md", S_IFREG | 0o0664, 0, 0);
-	const { fh } = await fs.open(file.id, 0);
-	const bytesWritten = await fs.write(file.id, fh, 0, contents);
+	const { fh } = await fs.open(file.attr.ino, 0);
+	const bytesWritten = await fs.write(file.attr.ino, fh, 0, contents);
 	console.log(`Wrote ${bytesWritten} bytes`);
-	const bytes = await fs.read(file.id, fh, 0, 512);
+	const bytes = await fs.read(file.attr.ino, fh, 0, 512);
 	const decoder = new TextDecoder();
 	console.log(`Saved file and read it back:\n\n${decoder.decode(bytes)}`);
-	await fs.release(file.id, fh, 0);
+	await fs.release(file.attr.ino, fh, 0);
 	localStorage.setItem("readme-written", "yes");
 }
 
 async function readDemo(fs: IdbFs) {
-	const rootDir = await fs.opendir(1, 0);
-	const files = await fs.readdir(1, rootDir.fh);
-	const readme = files.find((node) => node.name === "readme.md");
-	if (readme !== undefined && readme.type === S_IFREG) {
-		const { fh } = await fs.open(readme.ino, 0);
-		const readmeData = await fs.read(readme.ino, fh, 0, 512);
+	let readmeEntry: Entry | null;
+	try {
+		readmeEntry = await fs.lookup(1, "readme.md");
+	} catch(err) {
+		readmeEntry = null;
+	}
+	if (readmeEntry !== null && (readmeEntry.attr.mode & S_IFMT) === S_IFREG) {
+		const { fh } = await fs.open(readmeEntry.attr.ino, 0);
+		const readmeData = await fs.read(readmeEntry.attr.ino, fh, 0, 512);
 		const decoder = new TextDecoder();
 		const decodedMessage = decoder.decode(readmeData);
 		alert(`Found readme.md on the filesystem! It contains the following data:\n\n${decodedMessage}`);
