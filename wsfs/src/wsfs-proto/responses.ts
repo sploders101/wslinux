4ec37a90c1ccd0f7ed36ba4516b7285c60245dc4
Shaun Keys <mariobuddy@gmail.com>
2024-09-08 22:42:25 -0600

    Connect readlink, mknod, mkdir, unlink, rmdir, symlink, rename, and link

--- wsfs/src/wsfs-proto/responses.ts
+++ wsfs/src/wsfs-proto/responses.ts
@@ -1,17 +1,53 @@
-import { NodeAttr } from "../idbfs/types";
+import { Entry, NodeAttr } from "../idbfs/types";
 import { PacketBuilder } from "../packetizers";
 import { constants } from "./constants";
 
 /**
+ * Sends empty response to signal completion of the requested task
+ */
+export async function empty(ws: WebSocket, responseId: number) {
+	const packet = new PacketBuilder();
+	packet.u8(constants.actions.internals);
+	packet.u8(constants.internals.reply);
+	packet.u8(constants.replyTypes.empty);
+	packet.u16(responseId);
+	ws.send(packet.getPacket());
+}
+
+/**
  * Responds to a message with a nodestat and generation
  */
-export async function entry(responseId: number, ws: WebSocket, attr: NodeAttr, generation: number) {
-    // [response_type: u8][response_id: u16]
-    // [generation: u64][ino: u64][size: u64][blocks: u64][atimeMs: u64][mtimeMs: u64][ctimeMs: u64]
-    // [crtimeMs: u64][mode: u32][nlink: u32][uid: u32][gid: u32][rdev: u32][blksize: u32]
+export async function entry(ws: WebSocket, responseId: number, entry: Entry) {
     const packet = new PacketBuilder();
-    packet.u64(BigInt(generation));
+    packet.u8(constants.actions.internals);
     packet.u8(constants.internals.reply);
+    packet.u8(constants.replyTypes.entry);
+    packet.u16(responseId);
+    packet.u64(BigInt(entry.generation));
+    packet.u64(BigInt(entry.attr.ino));
+    packet.u64(BigInt(entry.attr.size));
+    packet.u64(BigInt(entry.attr.blocks));
+    packet.u64(BigInt(entry.attr.atimeMs));
+    packet.u64(BigInt(entry.attr.mtimeMs));
+    packet.u64(BigInt(entry.attr.ctimeMs));
+    packet.u64(BigInt(entry.attr.crtimeMs));
+    packet.u32(entry.attr.mode);
+    packet.u32(entry.attr.nlink);
+    packet.u32(entry.attr.uid);
+    packet.u32(entry.attr.gid);
+    packet.u32(entry.attr.rdev);
+    packet.u32(entry.attr.blksize);
+    ws.send(packet.getPacket());
+}
+
+/**
+ * Responds to a message with a nodestat
+ */
+export async function attr(ws: WebSocket, responseId: number, attr: NodeAttr) {
+    const packet = new PacketBuilder();
+    packet.u8(constants.actions.internals);
+    packet.u8(constants.internals.reply);
+    packet.u8(constants.replyTypes.attr);
     packet.u16(responseId);
     packet.u64(BigInt(attr.ino));
     packet.u64(BigInt(attr.size));
@@ -29,28 +65,17 @@ export async function entry(responseId: number, ws: WebSocket, attr: NodeAttr, g
     ws.send(packet.getPacket());
 }
 
-/**
- * Responds to a message with a nodestat
- */
-export async function attr(responseId: number, ws: WebSocket, attr: NodeAttr) {
-    // [response_type: u8][response_id: u16]
-    // [ino: u64][size: u64][blocks: u64][atimeMs: u64][mtimeMs: u64][ctimeMs: u64]
-    // [crtimeMs: u64][mode: u32][nlink: u32][uid: u32][gid: u32][rdev: u32][blksize: u32]
-    const packet = new PacketBuilder();
-    packet.u8(constants.internals.reply);
-    packet.u16(responseId);
-    packet.u64(BigInt(attr.ino));
-    packet.u64(BigInt(attr.size));
-    packet.u64(BigInt(attr.blocks));
-    packet.u64(BigInt(attr.atimeMs));
-    packet.u64(BigInt(attr.mtimeMs));
-    packet.u64(BigInt(attr.ctimeMs));
-    packet.u64(BigInt(attr.crtimeMs));
-    packet.u32(attr.mode);
-    packet.u32(attr.nlink);
-    packet.u32(attr.uid);
-    packet.u32(attr.gid);
-    packet.u32(attr.rdev);
-    packet.u32(attr.blksize);
-    ws.send(packet.getPacket());
+export function data(ws: WebSocket, responseId: number, data: string | Uint8Array) {
+	const packet = new PacketBuilder();
+	packet.u8(constants.actions.internals);
+	packet.u8(constants.internals.reply);
+	packet.u8(constants.replyTypes.data);
+	packet.u16(responseId);
+
+	if (typeof data === "string") {
+		packet.string(data);
+	} else {
+		packet.buffer(data);
+	}
+	ws.send(packet.getPacket());
 }
